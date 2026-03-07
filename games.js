12b8d3ffa6681ff8c8b9cabf3d4c4ed25395bac8
whispercross <whisper.cross@yandex.ru>
2026-03-07 16:15:13 +0300

    Second game developed

--- games.js
+++ games.js
@@ -32,6 +32,67 @@ function guessNumber() {
     }
 }
 //игра Простая арифметика
-function solveSimpleMaths() {
+function simpleMaths() {
     alert("Нужно решить простые арифметические примеры. Их будет пять.");
+    let rightAnswersCount = 0;
+    for (i = 1; i <= 5; i++) {
+        const mathOperators = ['+', '-', '*', '/'];
+        const randomIndex = Math.floor(Math.random() * mathOperators.length);
+        let mathExample = [Math.floor(Math.random() * 10 + 1),  mathOperators[randomIndex], Math.floor(Math.random() * 10 + 1)];
+        const userAnswer = Number(prompt("Пример " + i + ". " + mathExample[0] + " " + mathExample[1] + " " + mathExample[2] + " будет..."));
+        let answer;
+        if (mathOperators[randomIndex] == '+') {
+            answer = mathExample[0] + mathExample[2];
+            if (userAnswer == answer) {
+                alert("Правильно!");
+                rightAnswersCount++;
+            } else {
+                alert("Неправильно! Будет " + answer);
+            }
+        } else if (mathOperators[randomIndex] == '-') {
+            //проверка уменьшаемого и вычитаемого; разность должна быть целым положительным числом
+            if (mathExample[0] < mathExample[2]) {
+                do {
+                    mathExample.shift();
+                    mathExample.unshift(Math.floor(Math.random() * 10 + 1));
+                } while (mathExample[0] < mathExample[2]);
+            }
+            answer = mathExample[0] - mathExample[2];
+            if (userAnswer == answer) {
+                alert("Правильно!");
+                rightAnswersCount++;
+            } else {
+                alert("Неправильно! Будет " + answer);
+            }
+        } else if (mathOperators[randomIndex] == '*') {
+            answer = mathExample[0] * mathExample[2];
+            if (userAnswer == answer) {
+                alert("Правильно!");
+                rightAnswersCount++;
+            } else {
+                alert("Неправильно! Будет " + answer);
+            }
+        } else if (mathOperators[randomIndex] == '/') {
+            //проверка делимого и делителя; частное должно быть целым числом
+            if (mathExample[0] < mathExample[2]) {
+                do {
+                    mathExample.shift();
+                    mathExample.unshift(Math.floor(Math.random() * 10 + 1));
+                } while (mathExample[0] < mathExample[2]);
+            }
+            if (mathExample[0] % mathExample[2] !== 0) {
+                do {
+                    mathExample[0] = mathExample[0] + 1;
+                } while (mathExample[0] % mathExample[2] !== 0); //увеличить делимое на 1, пока остаток от деления не равен нулю
+            }
+            answer = mathExample[0] / mathExample[2];
+            if (userAnswer == answer) {
+                alert("Правильно!");
+                rightAnswersCount++;
+            } else {
+                alert("Неправильно! Будет " + answer);
+            }
+        }
+    }
+    alert("Вы решили правильно такое количество примеров: " + rightAnswersCount + ".");
 }
